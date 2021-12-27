af05f04bf7ef08a9661305a88e7445e7bf9643c5
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-12-28 00:40:31 +0200

    3

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express=require('express')
 var compression = require('compression')
-
+const path = require('path')
 const app = express()
 
 const port=process.env.PORT || 5000;
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client"));
+    app.get('*',(req,res)=>{
+        req.sendFile(path.resolve(__dirname,"client",'index.html'))
+    } )
+}
 app.listen(port,()=>{console.log("server connected")})
 app.use(compression())
 
