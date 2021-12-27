ab50f25c1665403ddf763ba2815b561b199f7a5a
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-12-28 01:54:08 +0200

    uhi

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var compression = require('compression')
 const path = require('path')
 const app = express()
 
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get('*',(req,res)=>{
+        req.sendFile(path.resolve(__dirname,"client/build",'index.html'))
+    } )
+}
 app.listen(process.env.PORT || 5000,()=>{console.log("server connected")})
 app.use(compression())
 
