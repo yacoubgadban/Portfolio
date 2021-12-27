ba94200a9693df9f9e8eb34c3a131174d2881ac1
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-12-28 01:37:22 +0200

    h

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ if(process.env.NODE_ENV === "production"){
 app.listen(process.env.PORT || 5000,()=>{console.log("server connected")})
 app.use(compression())
 
-app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extends :true}))
 
